4d568d7d5f80f9bc0e953b9dfb4d0c4fc5068df7
Ashutosh <31060855+AshutoshJha007@users.noreply.github.com>
2022-02-08 02:47:12 +0000

    linked list - append prepend and printList

--- linkedList.js
+++ linkedList.js
@@ -1,19 +1,47 @@
 // 10-->5-->7
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
   constructor(value){
-    this.head = {
-      value: value,
-      next: null
-    };
+    this.head = new Node(value);
     this.tail = this.head;
     this.length =1;
   }
 
   append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value){
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList(){
+    let array = [];
+    let currentNode = this.head;
+    while(currentNode != null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
   }
 }
 
 const myLinkedList = new LinkedList(10);
-
+myLinkedList.append(5);
+myLinkedList.prepend(7);
+myLinkedList.printList();
 console.log(myLinkedList);
