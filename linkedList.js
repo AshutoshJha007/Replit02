019ac8f210c69d472bea89c3b2b20ce1970ada59
Ashutosh <31060855+AshutoshJha007@users.noreply.github.com>
2022-02-09 02:34:20 +0000

    insert and remove for LinkedList

--- linkedList.js
+++ linkedList.js
@@ -38,10 +38,64 @@ class LinkedList {
     }
     console.log(array);
   }
+
+  insert(index, value){
+    if(index === 0){
+      return this.prepend(value);
+    }
+    if(index >= this.length){
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const headNode = this.traverseList(index-1);
+    const holdingNode = headNode.next;
+    headNode.next = newNode;
+    newNode.next = holdingNode;
+    this.length++;
+  
+  }
+
+  traverseList(index){
+    let currentNode = this.head;
+    let counter =0;
+    while(counter < index){
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  remove(index){
+    if(index === 0){
+      return this.head = this.head.next;
+    }
+    if(index > this.length){
+      return null;
+    }
+    const headNode = this.traverseList(index-1);
+    const tobeDeletedNode = headNode.next;
+    const joinNode = tobeDeletedNode.next;
+    headNode.next = joinNode;
+    this.length--;
+  }
+
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.prepend(7);
 myLinkedList.printList();
-console.log(myLinkedList);
+myLinkedList.insert(1,99);
+myLinkedList.printList();
+myLinkedList.insert(200,88);
+myLinkedList.printList();
+myLinkedList.insert(0,6);
+myLinkedList.printList();
+
+myLinkedList.remove(2);
+myLinkedList.printList();
+
+myLinkedList.remove(3);
+myLinkedList.printList();
+
+//console.log(myLinkedList);
