0d689bb7d753e623724efac47b2c17b6c9cab1e8
Ashutosh <31060855+AshutoshJha007@users.noreply.github.com>
2022-02-12 00:55:09 +0000

    linked list reverse again

--- linkedList.js
+++ linkedList.js
@@ -79,7 +79,23 @@ class LinkedList {
     this.length--;
   }
 
-  
+ reverse() {
+   if(!this.head.next){
+     return this.head;
+   }
+
+   let first = this.head;
+   this.tail = first;
+   let second = first.next;
+   while(second){
+     const temp = second.next;
+     second.next = first;
+     first = second;
+     second = temp;
+   }
+   this.head.next = null;
+   this.head = first;
+ }
 
 }
 
@@ -100,6 +116,8 @@ myLinkedList.printList();
 myLinkedList.remove(3);
 myLinkedList.printList();
 
+myLinkedList.reverse();
+myLinkedList.printList();
 myLinkedList.reverse();
 myLinkedList.printList();
 //console.log(myLinkedList);
