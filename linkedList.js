dbdad19f4444ca627c5caec55dc772bad700e613
Ashutosh <31060855+AshutoshJha007@users.noreply.github.com>
2022-02-12 00:44:07 +0000

    reverse gone

--- linkedList.js
+++ linkedList.js
@@ -79,22 +79,7 @@ class LinkedList {
     this.length--;
   }
 
-  reverse(){
-    if(!this.head.next){
-      return this.head
-    }
-    let first = this.head;
-    this.tail = this.head;
-    let second = this.head.next;
-    while(second){
-      const temp = second.next;
-      second.next = first;
-      first = second;
-      second = temp;
-    }
-    this.head.next = null;
-    this.head = first;
-  }
+  
 
 }
 
