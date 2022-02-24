35779ca48a21b06ca650bb722c230b4fd28c5faf
Ashutosh <31060855+AshutoshJha007@users.noreply.github.com>
2022-02-24 01:21:11 +0000

    Better Fibonacci

--- Algorithm/fibonachi.js
+++ Algorithm/fibonachi.js
@@ -28,3 +28,30 @@ function fibonacciRecursive(n) {
 }
 
 console.log(fibonacciRecursive(1))
+
+
+
+// Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+//For example: fibonacciRecursive(6) should return 8
+
+function fibonacciIterativeBetter(n){
+  let arr = [0,1]
+  for(i = 2; i <= n; i++){
+    arr.push(arr[i-1] + arr[i-2]);
+  }
+  return arr[n]
+}
+console.log(fibonacciIterativeBetter(5));
+
+function fibonacciRecursiveBetter(n) {
+  if( n < 2){
+    return n;
+  }
+  return fibonacciRecursiveBetter(n-1) + fibonacciRecursiveBetter(n-2)
+}
+
+console.log(fibonacciRecursiveBetter(5))
