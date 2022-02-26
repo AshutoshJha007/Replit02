81a0c22b505cb5e7b06e129167b0e86550bc2a47
Ashutosh <31060855+AshutoshJha007@users.noreply.github.com>
2022-02-26 00:44:23 +0000

    Enter sorting

---
+++ Algorithm/sorting/generalSorting.js
@@ -0,0 +1,10 @@
+let letters = ['w','e','r','l','g','h','i'];
+
+let numbers = [10,3,4,56,78,9,1]
+
+console.log( letters.sort() );
+
+console.log( numbers.sort() );
+
+console.log( numbers.sort((a,b) => (a-b)) );
+
