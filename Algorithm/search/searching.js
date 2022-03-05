2d1a4fab4c557b0eeb9573be65a65e5bb828ec1b
Ashutosh <31060855+AshutoshJha007@users.noreply.github.com>
2022-03-05 01:20:57 +0000

    search

---
+++ Algorithm/search/searching.js
@@ -0,0 +1,9 @@
+var beasts = ['Centaur', 'Godzilla', 'Mosura', 'Minotaur', 'Hydra', 'Nessie'];
+
+console.log(beasts.indexOf('Godzilla'));
+
+console.log(beasts.find(x => x === 'Godzilla'))
+
+console.log(beasts.findIndex( x => x === 'Godzilla'))
+
+console.log(beasts.includes('Godzilla'))
